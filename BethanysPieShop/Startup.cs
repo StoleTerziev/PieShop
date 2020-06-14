23c5d40c4273ce45dfcf772b3ee65021a98d27fb
stole terziev <stole.t@outlook.com>
2020-06-14 14:21:05 +0200

    Shopping Cart feature added

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -35,6 +35,10 @@ namespace BethanysPieShop
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IPieRepository, PieRepository>();
 
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
+
             services.AddControllersWithViews();//services.AddMvc(); would also work still
 
 
@@ -51,6 +55,7 @@ namespace BethanysPieShop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
